7d42d085739864b250a6c894b2599e36bfd440a1
Atish <maitreyaatish@gmail.com>
2018-11-19 21:15:38 -0800

    update api

--- api/src/atish/api/Data/main.go
+++ api/src/atish/api/Data/main.go
@@ -31,7 +31,7 @@ func Post(w http.ResponseWriter, r *http.Request) {
     if len(errs) == 0 {
 	    gocqlUuid = gocql.TimeUUID()
 	    json.NewEncoder(w).Encode(NewUserResponse{ID: gocqlUuid})
-	}
+	  }
 }
 
 func Get(w http.ResponseWriter, r *http.Request){
@@ -50,3 +50,38 @@ func Get(w http.ResponseWriter, r *http.Request){
    json.NewEncoder(w).Encode(data)
   }
 }
+
+
+func UpdateHandler(w http.ResponseWriter, r *http.Request){
+  var errs []string
+  var gocqlUuid gocql.UUID
+
+  r.ParseForm()
+  number := r.FormValue("number")
+  common_name :=r.FormValue("common_name")
+  if err := Cassandra.Session.Query("UPDATE cmpe281.cmpe281 set common_name=? WHERE number = ?",common_name,number).Exec(); err != nil {
+    errs = append(errs, err.Error())
+  }
+  if len(errs) == 0 {
+    gocqlUuid = gocql.TimeUUID()
+    json.NewEncoder(w).Encode(NewUserResponse{ID: gocqlUuid})
+  }
+}
+
+
+/*func DeleteHandler(w http.ResponseWriter, r *http.Request){
+  var errs []string
+  var gocqlUuid gocql.UUID
+  fmt.Println("here")
+  r.ParseForm()
+  number := r.FormValue("number")
+  // common_name :=r.FormValue("common_name")
+  fmt.Println(number);
+  if err := Cassandra.Session.Query("DELETE FROM cmpe281.cmpe281 WHERE number = ?",number).Exec(); err != nil {
+    errs = append(errs, err.Error())
+  }
+  if len(errs) == 0 {
+    gocqlUuid = gocql.TimeUUID()
+    json.NewEncoder(w).Encode(NewUserResponse{ID: gocqlUuid})
+  }
+}*/
