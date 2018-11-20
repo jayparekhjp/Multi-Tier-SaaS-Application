ffa7ab0eb8d41f721eedf74478b4ddee7d28f6e2
Yesha Dave <42763101+YeshaDave@users.noreply.github.com>
2018-11-20 12:12:46 -0800

    Put and Delete

--- api/src/atish/main.go
+++ api/src/atish/main.go
@@ -1,89 +1,127 @@
-/*package main
+package Data
 
 import (
-	"fmt"
-	"log"
-
-	"github.com/gocql/gocql"
-)
-
-func main() {
-	// connect to the cluster
-	cluster := gocql.NewCluster("18.144.62.162", "54.215.252.168", "13.57.7.146","52.53.128.5","13.56.81.44")
-	cluster.Keyspace = "cmpe281"
-	cluster.Consistency = gocql.Quorum
-	session, _ := cluster.CreateSession()
-	defer session.Close()
-
-	// var t,s,pk,v string
-	var common_name string
-
-	// list all tweets
-	for i := 0; i < 50; i++ {
-		iter := session.Query(`SELECT * FROM t`).Iter()
-		for iter.Scan(&common_name) {
-			fmt.Println("common_name:", common_name)
-		}
-		if err := iter.Close(); err != nil {
-			log.Fatal(err)
-		}
-	}
-
-	//Insert
-	for i := 0; i < 10; i++ {
-		if err := session.Query("INSERT INTO cmpe281 (number,common_name) VALUES (?,'test')", i).Exec(); err != nil {
-			log.Fatal(err)
-		}else{
-			fmt.Println("Inserted row:%d",i);		
-		}
-	}
-
-	//Update
-	var x string
-	x="test1"
-	if err := session.Query("UPDATE cmpe281 set common_name=?", x).Exec(); err != nil {
-			log.Fatal(err)
-	}
-
-	//Delete
-	var y string
-	y="test2"
-	if err := session.Query("DELETE *from cmpe281 where common_name=?", y).Exec(); err != nil {
-			log.Fatal(err)
-	}	
-	fmt.Println("Done")
-}
-*/
-
-package main
-
-import (
-  "net/http"
-  "log"
-  "encoding/json"
-  "github.com/gorilla/mux"
-   "atish/api/Cassandra"
-   "atish/api/Data"
+"net/http"
+"github.com/gocql/gocql"
+"encoding/json"
+"atish/api/Cassandra"
+"github.com/gorilla/mux"
+"fmt"
+"strconv"
 )
 
 
-type heartbeatResponse struct {
-  Status string `json:"status"`
-  Code int `json:"code"`
+type Identification struct {
+   number    int
+   common_name string
 }
 
-func main() {
-  CassandraSession := Cassandra.Session
-  defer CassandraSession.Close()
-  router := mux.NewRouter().StrictSlash(true)
-  router.HandleFunc("/", heartbeat)
-  router.HandleFunc("/data", Data.Post)
-  router.HandleFunc("/get/{number}", Data.Get)
-  router.HandleFunc("/data", Data.Update)
-  router.HandleFunc("/data", Data.Delete)
-  log.Fatal(http.ListenAndServe(":8080", router))
+
+func Post(w http.ResponseWriter, r *http.Request) {
+  	var errs []string
+  	var gocqlUuid gocql.UUID
+
+  	r.ParseForm()
+  	number := r.FormValue("number")
+  	common_name :=r.FormValue("common_name")
+  	// fmt.Println(r.FormValue("number"))
+  	fmt.Println(common_name)
+  	fmt.Println(strconv.Atoi(number))
+    if err := Cassandra.Session.Query("INSERT INTO cmpe281 (number,common_name) VALUES (?,?)", number,common_name).Exec(); err != nil {
+      errs = append(errs, err.Error())
+    } else {
+      // created := true
+    }
+    // generate a unique UUID for this user
+    if len(errs) == 0 {
+	    fmt.Println("creating a new user")
+	    gocqlUuid = gocql.TimeUUID()
+	    json.NewEncoder(w).Encode(NewUserResponse{ID: gocqlUuid})
+	}
+  /*
+
+
+    // write data to Cassandra
+  }
+
+  // depending on whether we created the user, return the
+  // resource ID in a JSON payload, or return our errors
+  if created {
+    fmt.Println("user_id", gocqlUuid)
+  } else {
+    fmt.Println("errors", errs)
+    json.NewEncoder(w).Encode(ErrorResponse{Errors: errs})
+  }*/
 }
 
-func heartbeat(w http.ResponseWriter, r *http.Request) {
-  json.NewEncoder(w).Encode(heartbeatResponse{Status: "OK", Code: 200})
+func Get(w http.ResponseWriter, r *http.Request){
+
+	var errs []string
+  	// var gocqlUuid gocql.UUID
+  	var idents []Identification
+
+  	vars := mux.Vars(r)
+    number := vars["number"]
+    var common_name string
+    // var jsonText = []byte(`[
+        // {""}]`)
+
+  	// number := r.FormValue("number")
+  	// common_name :=r.FormValue("common_name")
+  	// fmt.Println(r.FormValue("number"))
+  	// fmt.Println(common_name)
+  	// fmt.Println(strconv.Atoi(number))
+  	if err := Cassandra.Session.Query(`SELECT * FROM cmpe281.cmpe281 WHERE number = ?`,
+		number).Consistency(gocql.One).Scan(&number); err != nil {
+		errs = append(errs, err.Error())
+	}
+
+	iter := Cassandra.Session.Query(`SELECT * FROM cmpe281.cmpe281 WHERE number = ?`, number).Iter()
+	for iter.Scan(&common_name) {
+		fmt.Println("Tweet:", common_name)
+		// if err := json.Unmarshal([]byte(jsonText), &idents); err != nil {
+		// 		errs = append(errs, err.Error())
+		// }
+		// errs = append(errs, err.Error())
+		number_num,_ := strconv.Atoi(number)
+	    idents = append(idents, Identification{number: number_num, common_name: common_name})
+	}
+	if err := iter.Close(); err != nil {
+		errs = append(errs, err.Error())
+	}
+    // generate a unique UUID for this user
+    if len(errs) == 0 {
+	    // gocqlUuid = gocql.TimeUUID()
+	    result,_ := json.Marshal(idents)
+	    json.NewEncoder(w).Encode(result)
+	}
+
+
+}
+
+
+func Put(w http.ResponseWriter, r *http.Request) {
+	var errs []string
+	r.ParseForm()
+	number := r.FormValue("number")
+  common_name :=r.FormValue("common_name")
+	 fmt.Printf("Updating Emp with id = %s\n", emp.id)
+	 if err := Cassandra.Session.Query("UPDATE cmpe281.cmpe281 SET number = ?, common_name = ? WHERE number = ?",
+		 number,common_name).Exec(); err != nil {
+		 fmt.Println("Error while updating user")
+		 fmt.Println(err)
+	}
+}
+
+
+func Delete(w http.ResponseWriter, r *http.Request) {
+	var errs []string
+	r.ParseForm()
+	number := r.FormValue("number")
+  common_name :=r.FormValue("common_name")
+	fmt.Printf("Deleting User with number = %s\n", number_num)
+	  if err := Cassandra.Session.Query("DELETE FROM cmpe281.cmpe281 WHERE number = ?", number_num).Exec(); err != nil {
+		 fmt.Println("Error while deleting user")
+		 fmt.Println(err)
+	}
 }
