1282b7453cf7e9edac700becff839f03839e6648
Jay Parekh <jay.s.parekh@sjsu.edu>
2018-11-21 16:58:15 -0800

    go mongo api framework
    GO API for MongoDB. Functions yet to be implemented.

--- api/src/jayParekh/main.go
+++ api/src/jayParekh/main.go
@@ -1,46 +1,50 @@
 package main
 
 import (
-	
+	"fmt"
 	"log"
 	"net/http"
-	"gopkg.in/mgo.v2"
-	
+
 	"github.com/gorilla/mux"
 )
-	/*
-	type User struct {
-		ID		string	`json:"id"`
-		Name  	string	`json:"name"`
-	}
-	*/
-	type Burger struct {
-		Name  string
-	}
 
-func createBurger(w http.ResponseWriter, r *http.Request) {
+type User struct {
+	Id       string
+	Name     string
+	Password string
+}
 
-	r.ParseForm()
-	name := r.FormValue("name")
-	session, err := mgo.Dial("localhost:27017")
-	if err != nil {
-		panic(err)
-	}
-	defer session.Close()
+func ping(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("TO BE IMPLEMENTED")
+}
 
-	session.SetMode(mgo.Monotonic, true)
+func getUsers(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("TO BE IMPLEMENTED")
+}
 
-	c := session.DB("burgerPlace").C("burgers")
-	err = c.Insert(&Burger{Name: name})
-	/*
-		if err != nil {
-			log.Fatal
-		}
-	*/
+func getUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("TO BE IMPLEMENTED")
+}
+
+func createUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("TO BE IMPLEMENTED")
+}
+
+func upadteUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("TO BE IMPLEMENTED")
+}
+
+func deleteUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("TO BE IMPLEMENTED")
 }
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/api/burgers", createBurger).Methods("POST")
+	r.HandleFunc("/api/ping", ping).Methods("GET")
+	r.HandleFunc("/api/users", getUsers).Methods("GET")
+	r.HandleFunc("/api/users/{id}", getUser).Methods("GET")
+	r.HandleFunc("/api/users", createUser).Methods("POST")
+	r.HandleFunc("/api/users", updateUser).Methods("PUT")
+	r.HandleFunc("/api/users", deleteUSer).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":3000", r))
 }
