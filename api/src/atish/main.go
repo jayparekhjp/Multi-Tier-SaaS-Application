ab3e5421326ddf9a8bc063bc183924cb7da42f92
Atish <maitreyaatish@gmail.com>
2018-11-28 14:15:26 -0800

    Cassandra go api's

--- api/src/atish/main.go
+++ api/src/atish/main.go
@@ -2,27 +2,63 @@ package main
 
 import (
 	"fmt"
-
+	"net/http"
+    "log"
+    "os"
+    "errors"
+    // "encoding/json"
+    "github.com/gorilla/mux"
 	"github.com/gocql/gocql"
+	"strconv"
+	"github.com/unrolled/render"
+	"github.com/codegangsta/negroni"
 )
 
 var Session *gocql.Session
 
-type Emp struct {
-	restraunt_id string
-	restraunt_name string
-	restraunt_address string
-	zip_code  int
+
+type heartbeatResponse struct {
+  Status string `json:"status"`
+  Code int `json:"code"`
 }
 
-/*type Emp struct {
-	id        string
-	firstName string
-	lastName  string
-	age       int
-}*/
+type restrauntResponse struct{
+	 restraunt_id string `json:"restraunt_id""`  
+     name string `json:"name""`
+     address string `json:"address""`
+     phone string `json:"phone""`
+     open_hours string `json:"open_hours""`
+}
 
-func init() {
+type menuResponse struct{
+	 item_id string `json:"item_id""`  
+     name string `json:"name""`
+     price float32 `json:"price""`
+     restraunt_id string `json:"restraunt_id""`
+}
+
+func init() { log.SetFlags(log.Lshortfile | log.LstdFlags) }
+
+func callFunc() error {
+    return errors.New("error")
+}
+
+type finalRestrauntResponse struct{
+	restrauntResponse[] restrauntResponse
+}
+
+type finalMenuResponse struct{
+	menuResponse[] menuResponse
+}
+
+func NewServer() *negroni.Negroni {
+	formatter := render.New(render.Options{
+		IndentJSON: true,
+	})
+	n := negroni.Classic()
+	mx := mux.NewRouter()
+	initRoutes(mx, formatter)
+	n.UseHandler(mx)
 	var err error
 
 	cluster := gocql.NewCluster("127.0.0.1")
@@ -32,86 +68,106 @@ func init() {
 		panic(err)
 	}
 	fmt.Println("cassandra init done")
+	return n
+}
+
+
+func initRoutes(mx *mux.Router, formatter *render.Render) {
+	mx.HandleFunc("/ping", pingHandler(formatter)).Methods("GET")
+	mx.HandleFunc("/data", restrauntHandler(formatter)).Methods("GET")
+	mx.HandleFunc("/menu", menuHandler(formatter)).Methods("GET")
+	// mx.HandleFunc("/order", gumballNewOrderHandler(formatter)).Methods("POST")
+	// mx.HandleFunc("/order/{id}", gumballOrderStatusHandler(formatter)).Methods("GET")
+	// mx.HandleFunc("/order", gumballOrderStatusHandler(formatter)).Methods("GET")
+	// mx.HandleFunc("/orders", gumballProcessOrdersHandler(formatter)).Methods("POST")
+}
+
+func pingHandler(formatter *render.Render) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		formatter.JSON(w, http.StatusOK, struct{ Test string }{"API is alive!"})
+	}
+}
+
+func restrauntHandler(formatter *render.Render) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		vars := req.URL.Query()
+		var zip int
+		zip,_ = strconv.Atoi(vars.Get("zip"))
+		var restraunt_name string
+		var restraunt_address string
+		var phone_number string
+		var opening_hours string
+		var restraunt_id string
+		if err := Session.Query("SELECT restraunt_id,restraunt_name,restraunt_address,phone_number,opening_hours FROM restraunts WHERE zip_code = ? ALLOW FILTERING",zip).Consistency(gocql.One).Scan(&restraunt_id,&restraunt_address,&restraunt_name,&phone_number,&opening_hours); err != nil {
+			log.Fatal(err)
+		}
+		iter := Session.Query("SELECT restraunt_id,restraunt_name,restraunt_address,phone_number,opening_hours FROM restraunts WHERE zip_code = ? ALLOW FILTERING",zip).Iter()
+		var restraunt_array []restrauntResponse
+		var data restrauntResponse
+		// var data = make(map[string]restrauntResponse)
+		for iter.Scan(&restraunt_id,&restraunt_address,&restraunt_name,&phone_number,&opening_hours) {
+			data = restrauntResponse{
+				restraunt_id : restraunt_id,    
+				name :         restraunt_name,
+				address :      restraunt_address,
+				phone :        phone_number,
+				open_hours :   opening_hours,
+			}
+			restraunt_array = append(restraunt_array, data)
+
+		}
+
+		fmt.Printf("%+v", restraunt_array)
+		w.Header().Set("Content-Type", "application/json")
+		// var final,_ = json.Marshal(restraunt_array)
+		// fmt.Println(string(final))
+		formatter.JSON(w, http.StatusOK, restraunt_array)
+	}
+}
+
+
+func menuHandler(formatter *render.Render) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		vars := req.URL.Query()
+		var res_id string
+		res_id = vars.Get("restraunt_id")
+		var restraunt_id string
+		var item_id string
+		var price float32
+		var name string
+		if err := Session.Query("SELECT item_id,name,price,restraunt_id FROM menu WHERE restraunt_id = ? ALLOW FILTERING",res_id).Consistency(gocql.One).Scan(&item_id,&name,&price,&restraunt_id); err != nil {
+			log.Fatal(err)
+		}
+		iter := Session.Query("SELECT item_id,name,price,restraunt_id FROM menu WHERE restraunt_id = ? ALLOW FILTERING",res_id).Iter()
+		var restraunt_array []menuResponse
+		var data menuResponse
+		// var data = make(map[string]restrauntResponse)
+		for iter.Scan(&item_id,&name,&price,&restraunt_id) {
+			data = menuResponse{
+				item_id : item_id,    
+				name :                name,
+				price :      		  price,
+				restraunt_id :        restraunt_id,
+			}
+			restraunt_array = append(restraunt_array, data)
+			fmt.Printf("%+v", restraunt_array)
+		}
+
+		// fmt.Printf("%+v", restraunt_array)
+		// var final,_ = json.Marshal(restraunt_array)
+		// fmt.Println(string(final))
+		formatter.JSON(w, http.StatusOK, restraunt_array)
+	}
 }
 
 func main() {
-	// emp1 := Emp{"1399a878-7e0d-4439-9dd4-32ad58dd0cd2", "Anupam", "Raj", 20}
-	// emp2 := Emp{"E-2", "Rahul", "Anand", 30}
-	// createEmp(emp1)
-	// fmt.Println(getEmps())
-	// createEmp(emp2)
-	fmt.Println(getEmps())
-	// emp3 := Emp{"E-1", "Rahul", "Anand", 30}
-	// updateEmp(emp3)
-	// fmt.Println(getEmps())
-	// deleteEmp("E-2")
-	// fmt.Println(getEmps())
+
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "3000"
+	}
+
+	server := NewServer()
+	server.Run(":" + port)
 }
 
-/*func getEmps() []Emp {
-	fmt.Println("Getting all Employees")
-	var emps []Emp
-	m := map[string]interface{}{}
-
-	iter := Session.Query("SELECT * FROM emps").Iter()
-	for iter.MapScan(m) {
-		emps = append(emps, Emp{
-			id:        m["empid"].(string),
-			firstName: m["first_name"].(string),
-			lastName:  m["last_name"].(string),
-			age:       m["age"].(int),
-		})
-		m = map[string]interface{}{}
-	}
-
-	return emps
-}*/
-
-func getEmps() []Emp {
-	var emps []Emp
-	// m := map[string]interface{}{}
-	var restraunt_name string
-	var restraunt_address string
-	iter := Session.Query("SELECT * FROM restraunts").Iter()
-	for {
-		// New map each iteration
-		row := map[string]interface{}{
-			restraunt_name: &restraunt_name,
-			restraunt_address:  &restraunt_address,	
-		}
-		if !iter.MapScan(row) {
-			break
-		}
-		fmt.Printf("First: %s Address: %s\n", restraunt_name, restraunt_address)
-	}
-	for iter.Scan(&restraunt_address,&restraunt_name) {
-	   // fmt.Println("number:", number_num)
-	   /*var  data = Identification{
-	    number : number_num,
-	    name : common_name,
-	   }*/
-	   fmt.Println(restraunt_name)
-	   fmt.Println(restraunt_address)
-	   // json.NewEncoder(w).Encode(data)
-	  }
-	/*for iter.MapScan(m) {
-		emps = append(emps, Emp{
-			// restraunt_id:        m["restraunt_id"].(string),
-			restraunt_name: m["restraunt_name"].(string),
-			restraunt_address:  m["restraunt_address"].(string),
-			zip_code:       m["zip_code"].(int),
-		})
-		m = map[string]interface{}{}
-	}*/
-	fmt.Println(iter.NumRows())
-	return emps
-}
-
-/*func createEmp(emp Emp) {
-	fmt.Println(" **** Creating new emp ****\n", emp)
-	if err := Session.Query("INSERT INTO restraunt(empid, first_name, last_name, age) VALUES(?, ?, ?, ?)",
-		emp.id, emp.firstName, emp.lastName, emp.age).Exec(); err != nil {
-		fmt.Println("Error while inserting Emp")
-		fmt.Println(err)
-	}
-}*/
