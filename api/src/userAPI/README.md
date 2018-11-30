aa4e4c4dd91dd4064fe0eab7003a96d67dc4858a
Jay Parekh <jay.s.parekh@sjsu.edu>
2018-11-29 23:54:04 -0800

    userAPI logics corrected
    login logics:
    function should return userid
    
    signup logics:
    username should be unique
    
    admin:
    admin should get all data

--- api/src/userAPI/README.md
+++ api/src/userAPI/README.md
@@ -1,7 +1,5 @@
 # User API
 
-### Description
-
 API made in Golang to access MongoDB and provide user functionalities like Signup, Login.
 
 ## User API Version 1
@@ -11,3 +9,8 @@ Simple Golang API using **mux** router and **mgo** for handling MongoDB.
 ## User API Version 2
 
 Added **negroni** server.
+
+### External Packages
+
+***mgo:** <https://godoc.org/gopkg.in/mgo.v2>*
+***negroni:** <https://godoc.org/github.com/codegangsta/negroni>*
--- api/src/userAPI/userAPIv2.go
+++ api/src/userAPI/userAPIv2.go
@@ -1,6 +1,6 @@
-// Version 2 API
+// Version 3 API
 // Using Negroni
-// Reference:<https://github.com/paulnguyen/cmpe281/blob/master/golabs/gocloud/gumball/goapi/goapi_v3/src/gumball/server.go>
+
 package main
 
 import (
@@ -21,6 +21,11 @@ var (
 	collection *mgo.Collection
 )
 
+// MongoDB connections
+var mongoServer = "localhost:27017"
+var mongoDatabase = "counterBurger"
+var mongoCollection = "users"
+
 // NewServer configures and returns a server
 func NewServer() *negroni.Negroni {
 	formatter := render.New(render.Options{
@@ -36,18 +41,22 @@ func NewServer() *negroni.Negroni {
 // User struct defines elements of user
 type User struct {
 	ID        bson.ObjectId `bson:"_id" json:"id"`
+	Username  string        `bson:"username" json:"username"`
 	Name      string        `json:"name"`
 	Password  string        `json:"password"`
 	TimeStamp time.Time     `json:"timestamp"`
-	Contact   int           `json:"contact"`
+	Contact   string        `json:"contact"`
 	Address   string        `json:"address"`
 }
 
 // initRoutes for setting the API endpoint routes
 func initRoutes(mx *mux.Router, formatter *render.Render) {
 	mx.HandleFunc("/api/ping", ping(formatter)).Methods("GET")
+	mx.HandleFunc("/api/users", getAllUsers(formatter)).Methods("GET")
 	mx.HandleFunc("/api/users/signup", signup(formatter)).Methods("POST")
 	mx.HandleFunc("/api/users/login", login(formatter)).Methods("GET")
+	mx.HandleFunc("/api/users/{id}", updatePassword(formatter)).Methods("PUT")
+	mx.HandleFunc("/api/users/{id}", deleteAccount(formatter)).Methods("DELETE")
 }
 
 // ping function for checking the status of the API
@@ -57,33 +66,71 @@ func ping(formatter *render.Render) http.HandlerFunc {
 	}
 }
 
+// getAllUsers function for testing purposes only
+func getAllUsers(formatter *render.Render) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		var reqUser User
+		var users []User
+		err := json.NewDecoder(req.Body).Decode(&reqUser)
+		if reqUser.Username == "admin" && reqUser.Password == "cmpe281" {
+			formatter.JSON(w, http.StatusOK, "Welcome Master")
+			iter := collection.Find(nil).Iter()
+			result := User{}
+			for iter.Next(&result) {
+				users = append(users, result)
+			}
+			j, err := json.Marshal(users)
+			if err != nil {
+				panic(err)
+			}
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(j)
+
+		} else {
+			formatter.JSON(w, http.StatusOK, "You are not admin.")
+			return
+		}
+		if err != (nil) {
+			panic(err)
+		}
+	}
+}
+
 // signup function fot creatinmg a new user
 func signup(formatter *render.Render) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
 		var user User
+		var testUser User
 		err := json.NewDecoder(req.Body).Decode(&user)
-
-		// Generate a new ID
-		objID := bson.NewObjectId()
-		// Transfer this ID to user
-		user.ID = objID
-		// Give TimeStamp to user
-		user.TimeStamp = time.Now()
-		// Insert user into collection
-		err = collection.Insert(&user)
-		if err != nil {
-			panic(err)
+		// Check if the username already exists
+		// testUser.Username = user.Username
+		err = collection.Find(bson.M{"username": user.Username}).One(&testUser)
+		if testUser.Username != "" {
+			formatter.JSON(w, http.StatusOK, "Username taken. Please try different username.")
+			return
 		} else {
-			log.Printf("Successfully created User: %s", user.Name)
+			// Generate a new ID
+			objID := bson.NewObjectId()
+			// Transfer this ID to user
+			user.ID = objID
+			// Give TimeStamp to user
+			user.TimeStamp = time.Now()
+			// Insert user into collection
+			err = collection.Insert(&user)
+			if err != nil {
+				panic(err)
+			} else {
+				log.Printf("Successfully created User: %s", user.Name)
+			}
+			j, err := json.Marshal(user)
+			if err != nil {
+				panic(err)
+			}
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(j)
+			formatter.JSON(w, http.StatusOK, "Welcome to Counter Burger")
+			return
 		}
-		j, err := json.Marshal(user)
-		if err != nil {
-			panic(err)
-		}
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(j)
-		formatter.JSON(w, http.StatusOK, "Welcome to Counter Burger")
-		return
 	}
 }
 
@@ -93,11 +140,13 @@ func login(formatter *render.Render) http.HandlerFunc {
 		var reqUser User
 		var user User
 		err := json.NewDecoder(req.Body).Decode(&reqUser)
-		err = collection.Find(bson.M{"name": reqUser.Name}).One(&user)
+		err = collection.Find(bson.M{"username": reqUser.Username}).One(&user)
 		if reqUser.Password != user.Password {
 			formatter.JSON(w, http.StatusOK, "Password Incorrect")
+			return
 		} else if reqUser.Password == user.Password {
-			formatter.JSON(w, http.StatusOK, "Login Successful")
+			formatter.JSON(w, http.StatusOK, user.ID)
+			return
 		}
 		if err != (nil) {
 			panic(err)
@@ -105,17 +154,33 @@ func login(formatter *render.Render) http.HandlerFunc {
 	}
 }
 
+// updatePassword function for updating the password
+func updatePassword(formatter *render.Render) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		formatter.JSON(w, http.StatusOK, "updatePassword function not implemented yet")
+		return
+	}
+}
+
+// deleteAccount function for updating the password
+func deleteAccount(formatter *render.Render) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		formatter.JSON(w, http.StatusOK, "deleteAccount function not implemented yet")
+		return
+	}
+}
+
 // main function for creating the server and running it
 func main() {
 
 	var err error
-	session, err = mgo.Dial("localhost:27017")
+	session, err = mgo.Dial(mongoServer)
 	if err != nil {
 		panic(err)
 	}
 	defer session.Close()
 	session.SetMode(mgo.Monotonic, true)
-	collection = session.DB("counterBurger").C("users")
+	collection = session.DB(mongoDatabase).C(mongoCollection)
 
 	server := NewServer()
 	server.Run(":" + "3000")
