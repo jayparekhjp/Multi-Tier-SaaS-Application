9fa59ce61f1766e71fcced6ea8ddee63084d7986
pateljay684 <37415064+pateljay684@users.noreply.github.com>
2018-12-03 14:41:56 -0800

    change in nodejs for route
    app.js chnage

--- node/app.js
+++ node/app.js
@@ -1,57 +1,34 @@
 var express=require("express");
 var bodyParser=require('body-parser');
-const port = 8012;
-// var connection = require('./config');
+const port = 8000;
 var app = express();
+var Client = require('node-rest-client').Client;
 app.set('view engine', 'ejs');
 
 var request = require('request');
 var http = require('http');
-var cookie = require('cookie');
-
 
 app.use(express.static(__dirname + '/public'));
-
-// var authenticateController=require('./controllers/authenticate-controller');
-// var registerController=require('./controllers/register-controller');
-function parseCookies (request) {
-    var list = {},
-        rc = request.headers.cookie;
-
-    rc && rc.split(';').forEach(function( cookie ) {
-        var parts = cookie.split('=');
-        list[parts.shift().trim()] = decodeURI(parts.join('='));
-    });
-
-    return list;
-}
-
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', function (req, res) {
-    var cookies = parseCookies(req);  
-   var name = cookies.username;
-   console.log(name);
+
    res.render('index',{
-    name : name
    });
 })  
- 
-app.get('/login', function (req, res) {  
-   res.sendFile( __dirname + "/views/" + "login.html" );  
-})  
- 
-/* route to handle login and registration */
-// app.post('/api/register',registerController.register);
-// app.post('/api/authenticate',authenticateController.authenticate);
- 
-// console.log(authenticateController);
-// app.post('/controllers/register-controller', registerController.register);
-// app.post('/controllers/authenticate-controller', authenticateController.authenticate);
+
+app.get('/cart', function (req, res) {
+    var client = new Client();
+    client.get("http://demo8655652.mockable.io/menus", function (data, response) {
+        // console.log(data[0]['name']);
+        res.render('menu',{
+          data:data
+        });
+    });
+});
+
 app.listen(port, function () {
   console.log("Server is running on "+ port +" port");
 });
-
-
