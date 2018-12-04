f8ce45dac58ce8fb48563ecafcc7e463d636c959
pateljay684 <37415064+pateljay684@users.noreply.github.com>
2018-12-04 01:33:19 -0800

    view cart route and delete route

--- node/app.js
+++ node/app.js
@@ -1,34 +1,79 @@
 var express=require("express");
 var bodyParser=require('body-parser');
-const port = 8000;
+const port = 8012;
+// var connection = require('./config');
 var app = express();
-var Client = require('node-rest-client').Client;
 app.set('view engine', 'ejs');
+app.set('views','./views');
+
+var Client = require('node-rest-client').Client;
+var client = new Client();
 
 var request = require('request');
+var Cookies = require('cookies')
 var http = require('http');
+var cookie = require('cookie');
+var Client = require('node-rest-client').Client;
+var keys = ['keyboard cat']
+
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true })); 
 
-app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.get('/', function (req, res) {
 
-   res.render('index',{
-   });
-})  
 
-app.get('/cart', function (req, res) {
+app.get('/viewcart', function (req, res) {
     var client = new Client();
-    client.get("http://demo8655652.mockable.io/menus", function (data, response) {
-        // console.log(data[0]['name']);
-        res.render('menu',{
+    var args = {
+        data: { "id": "2" },
+        headers: {"Content-Type":"application/json"}// request headers
+    };
+    client.get("http://localhost:3000/api/cart/itemDisplay",args, function (data, response) {
+        res.render('cart',{
           data:data
         });
     });
 });
 
+app.post('/deleteitem',function(req,res){
+  var args = {
+      data: { 
+          id : req.body.userid,
+          iid : req.body.itemid,
+          rid : req.body.resid 
+      },
+      headers: { "Content-Type": "application/json" }
+  };
+  console.log(args);
+  client.delete("http://localhost:3000/api/cart/cartDelete", args, function (data, response) {
+      
+          res.redirect('/viewcart');
+      }
+  );
+});
+
+
+app.get('/payment', function (req,res) {
+    //var client = new Client();
+    //client.post("http://localhost:3002/orders", function (data, response) {
+        //console.log(data[0]['name']);
+        res.render('payment');
+    });
+
+
+app.post('/order', function (req, res) {
+    var client = new Client();
+    var args = {
+        headers: { "Content-Type": "application/json" }
+    };    
+     client.post("http://localhost:3002/orders",args, function (data, response) {
+      // console.log(data[0]['name']);
+    });
+});
+    
 app.listen(port, function () {
   console.log("Server is running on "+ port +" port");
 });
+
