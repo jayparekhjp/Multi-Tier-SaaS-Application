90d2233ea87085d2d8a1111a9ed0eb0bd33cf6bb
Jay Parekh <jay.s.parekh@sjsu.edu>
2018-12-05 22:56:34 -0800

    ejs changes for userAPI frontend

--- node/app.js
+++ node/app.js
@@ -43,7 +43,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', function (req, res) {
    var cookies = parseCookies(req);  
    var name = cookies.username;
-   console.log(name);
    res.render('home',{
     name : 'hello'
    });
@@ -52,7 +51,7 @@ app.get('/', function (req, res) {
 // Jay Parekh-Users ----------------------------------------------------------------------------------------------------
 app.get('/users/ping',function(req,res){
     console.log('Ping Called');
-    client.get("http://localhost:3000/api/ping", function (data, response) {
+    client.get("http://13.56.115.80:3000/api/ping", function (data, response) {
     // client.methods.pingUserAPI(function (data, response) {
         console.log(data);
         res.send(data);
@@ -65,13 +64,13 @@ app.get('/login',function(req,res){
     });
 });
 
-app.get('/login2',function(req,res){
-    res.render('login2');
-});
+// app.get('/login-test',function(req,res){
+//     res.render('login-test');
+// });
 
-app.get('/signup2',function(req,res){
-    res.render('signup2');
-});
+// app.get('/signup-test',function(req,res){
+//     res.render('signup-test');
+// });
 
 app.post('/users/loginSubmit',function(req,res){
     var args = {
@@ -82,10 +81,12 @@ app.post('/users/loginSubmit',function(req,res){
         headers: { "Content-Type": "application/json" }
     };
     console.log(args);
-    client.post("http://localhost:3000/api/users/login", args, function (data, response) {
+    client.post("http://13.56.115.80:3000/api/users/login", args, function (data, response) {
         console.log(data);
         // res.send(data);
-        if (data){
+        if (data == "Password Incorrect"){
+            res.redirect('/login');
+        }else if (data){    
             var cookies = new Cookies(req, res, { keys: keys})
             cookies.set('userid', data, {signed: true})
             res.redirect('/restraunts');
@@ -103,7 +104,7 @@ app.post('/users/loginSubmit',function(req,res){
 //     });
 // });
 
-app.get('/users/signup',function(req,res){
+app.get('/signup',function(req,res){
     res.render('signup', {
         title: 'Signup | Counter Burger'
     });
@@ -121,7 +122,7 @@ app.post('/users/signupSubmit',function(req,res){
         headers: { "Content-Type": "application/json" }
     };
     console.log(args);
-    client.post("http://localhost:3000/api/users/signup", args, function (data, response) {
+    client.post("http://13.56.115.80:3000/api/users/signup", args, function (data, response) {
         // parsed response body as js object
         console.log(data);
         if (data == 1){
